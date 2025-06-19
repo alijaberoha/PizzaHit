531fc430f3a5a9b9a8f7ff940c6200db959c6cb4
Ali <alijaberoha@hotmail.com>
2025-06-19 09:55:38 +0200

    add carte

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Navbar from './components/nav/Nav'
-
+import Home from "../src/features/home/Home"
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <Home/>
     </>
   )
 }
